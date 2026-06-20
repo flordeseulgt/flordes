bb83f5d90e32b2ab0aae14b4faa3dcb351ac38ef
flordeseulgt <flordeseulgt@gmail.com>
2026-06-19 22:54:34 -0600

    agrega script de Microsoft Clarity para mapas de calor

--- flords-next/src/app/layout.js
+++ flords-next/src/app/layout.js
@@ -94,6 +94,13 @@ export default function RootLayout({ children }) {
             } catch (e) {}
           })();
         ` }} />
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "x9tr06o0rq");
+        ` }} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
